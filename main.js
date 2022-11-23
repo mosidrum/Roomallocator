5c3dee709e238ac1e8f3cfb43ff1c45123e2bc76
mosidrum <mosiokanga@gmail.com>
2022-11-23 15:57:21 +0100

    we will continue after church

--- main.js
+++ main.js
@@ -26,3 +26,39 @@ document.querySelector(".comeup .close-btn").addEventListener("click", function 
 document.querySelector(".rooms .close").addEventListener("click", function () {
     document.querySelector(".rooms").classList.remove("active");
 });
+
+
+// DOM Element
+const allocate = e => {
+
+   let devData = JSON.parse(localStorage.getItem('devData')) || [];
+    devData.push({
+            name: document.getElementById('name').value,
+            gender: document.getElementById('gender').value,
+            type: document.getElementById('type').value,
+            skillset: document.getElementById('skillset').value
+
+    });
+
+    localStorage.setItem('devData', JSON.stringify(devData));
+    displayDevData();
+    // console.log(localStorage.getItem('devData'));
+    document.querySelector('form').reset();
+    e.preventDefault();
+}
+
+function displayDevData() {
+    console.log(localStorage.getItem('devData'));
+    if (localStorage.getItem('devData')) {
+        var output = document.querySelector('.rooms');
+        output.innerHTML = "";
+        JSON.parse(localStorage.getItem('devData')).forEach(data => {
+            output.innerHTML += `
+            <div>
+                <label>Name</label>
+            </div>
+            `;
+        });
+    }
+}
+
